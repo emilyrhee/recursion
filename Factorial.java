2ed17ac22757f7e36a804b92514127ceda43e961
emilyrhee <emilyrhee99@gmail.com>
2023-09-23 12:03:56 -0400

    implemented factorial of nth number

--- Factorial.java
+++ Factorial.java
@@ -1,4 +1,12 @@
 public class Factorial {
+    static int factorial(int n) {
+        if (n == 0 || n == 1) 
+            return 1;
+        
+        return n * factorial(n - 1);
+    }
     public static void main(String[] args) {
+        int n = 3;
+        System.out.println(n + "! = " + factorial(n));
     }
 }
